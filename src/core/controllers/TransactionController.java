627f55ed8e16194ef4db1bf848bf4a74eb0495ce
Paula Fernandez <fernandezlizarazop@gmail.com>
2024-11-18 17:08:48 -0500

    Se crearon las clases de los controladores y se trabajo en la de User

--- src/core/controllers/TransactionController.java
+++ src/core/controllers/TransactionController.java
@@ -2,14 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package bank;
+package core.controllers;
+
 
 /**
  *
- * @author edangulo
+ * @author ferna
  */
-public enum TransactionType {
-    DEPOSIT,
-    WITHDRAW,
-    TRANSFER
+public class TransactionController {
+    
 }
